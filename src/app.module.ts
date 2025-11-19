85b0c3c1929ce3592f6ac3e6bc5fcee3adbbe848
swlee <swo98@naver.com>
2025-11-19 20:07:10 +0900

    resotre: nestjs 깔고 app.module 복구

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "./auth/auth.module";
+import { ChatModule } from "./chat/chat.module";
+import { User } from "./users/user.entity";
+import { InterpretationModule } from "./interpretation/interpretation.module";
+import { openAIConfig } from "./external/openai/openai.config";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [openAIConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get<string>("DB_HOST", "localhost"),
+        port: Number(configService.get<string>("DB_PORT", "5432")),
+        username: configService.get<string>("DB_USER", "postgres"),
+        password: configService.get<string>("DB_PASSWORD", "postgres"),
+        database: configService.get<string>("DB_NAME", "dream_decoder"),
+        entities: [User],
+        synchronize: true,
+      }),
+    }),
+    AuthModule,
+    ChatModule,
+    InterpretationModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
