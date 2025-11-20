b8e8727419ce3af03f777604ffa4175cda7a127e
swlee <swo98@naver.com>
2025-11-20 19:39:41 +0900

    comment: 참고링크 주석 추가

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,6 +1,6 @@
-import 'express';
+import "express";
 
-declare module 'express' {
+declare module "express" {
   interface Request {
     user?: {
       id: string;
@@ -8,3 +8,5 @@ declare module 'express' {
     };
   }
 }
+// nestjs에서 request 타입 확장 참고 링크
+// https://medium.com/@letnaturebe2/nestjs%EC%97%90%EC%84%9C-request%EB%A5%BC-%EC%8A%A4%EB%A7%88%ED%8A%B8%ED%95%98%EA%B2%8C-%ED%99%95%EC%9E%A5%ED%95%98%EA%B8%B0-f2a78cc7e652
